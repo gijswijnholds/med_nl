47e342b4d63732af6d326e409c10fc394457fdc5
gijswijnholds <gijswijnholds@gmail.com>
2022-10-12 20:52:52 +0200

    Add analysis by feature

--- src/analysis.py
+++ src/analysis.py
@@ -14,9 +14,23 @@ def compute_accuracy(agg_results: List[Tuple[ProcessedSample, List[int]]]) -> fl
         return -1
 
 
+all_med_features = ['core args', 'disjunction', 'reverse', 'restrictivity', 'relative clauses', 'quantifiers',
+                    'conjunction', 'existential', 'intervals/numbers', 'world knowledge', 'redundancy',
+                    'named entities', 'anaphora', 'morphological negation', 'lexical_knowledge', 'conjunction',
+                    'common sense', 'conditionals', 'negation', 'npi', 'intersectivity', 'other']
+
+def filter_by_feature(agg_results: List[Tuple[ProcessedSample, List[int]]], feature: str):
+    return [sp for sp in agg_results if feature in sp[0].compact.features]
+
+def filter_by_features(agg_results: List[Tuple[ProcessedSample, List[int]]], features: List[str]):
+    return [sp for sp in agg_results if any(map(lambda f: f in sp[0].compact.features, features))]
+
+
 def agg_analysis(agg_results: List[Tuple[ProcessedSample, List[int]]]):
     accuracy_total = compute_accuracy(agg_results)
     accuracy_up = compute_accuracy([sp for sp in agg_results if sp[0].compact.mono == 'UP'])
     accuracy_down = compute_accuracy([sp for sp in agg_results if sp[0].compact.mono == 'DOWN'])
     accuracy_non = compute_accuracy([sp for sp in agg_results if sp[0].compact.mono == 'NON'])
-    return {'total': accuracy_total, 'up': accuracy_up, 'down': accuracy_down, 'non': accuracy_non}
+    accuracy_feature = {feature: compute_accuracy(filter_by_feature(agg_results, feature)) for feature in all_med_features}
+    return {'total': accuracy_total, 'up': accuracy_up, 'down': accuracy_down, 'non': accuracy_non,
+            'feature': accuracy_feature}
